126273a24f66d5c29d3ea528d1a99203341ad39d
Adam Eaton <adameaton55@gmail.com>
2018-01-06 18:55:44 -0600

    sql injection works
    need to limit sql commands

--- 07-sql-vaccine/app.js
+++ 07-sql-vaccine/app.js
@@ -13,33 +13,51 @@ let sqlite3 = require('sqlite3');
 let port = 4719;
 let app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 let db = new sqlite3.Database(':memory:');
 
+// Database Users
+let dbVals = [
+    { user: 'john', pass: 'smith'},
+    { user: 'admin', pass: 'securepass'},
+    { user: 'flag', pass: 'flag{0h_1nj3ct10n_s0_fancy}'}
+];
+
+// Populate database
 db.serialize(() => {
-    db.run('CREATE TABLE test (info TEXT)');
+    db.run('CREATE TABLE users (user TEXT, pass TEXT)');
 
-    let query = db.prepare('INSERT INTO test VALUES (?)');
-    for (let i = 0; i < 10; i++) {
-        query.run('test: ' + i);
-    }
-    query.finalize();
-
-    db.each('SELECT * FROM test', (err, row) => {
-        console.log(row);
+    let query = db.prepare('INSERT INTO users VALUES (?, ?)');
+    
+    dbVals.forEach(u => {
+        query.run(u.user, u.pass);
     });
+
+    query.finalize();
 });
 
+// Index page route
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/login', (req, res) => {
-    let user = req.params.user
-    console.log(req.params);
-    res.json({});
+// Login Route
+app.use('/login', (req, res) => {
+    let user = req.body.user.toLowerCase();
+    let pass = req.body.pass.toLowerCase();
+
+    db.all('SELECT * FROM users WHERE user=\'' + user + '\' AND pass=\'' + pass + '\'', (err, row) => {
+        res.json(row);
+    });
+});
+
+// Users Route
+app.use('/users', (req, res) => {
+    db.all('SELECT user FROM users', (err, row) => {
+        res.json(row);
+    });
 });
 
 app.listen(port);
-console.log('Challenge 6 Running... on port ' + port);
+console.log('Challenge 7 Running... on port ' + port);
